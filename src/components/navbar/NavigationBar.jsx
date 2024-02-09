394c697c9840f26df05079ebfdb8f245601fe7af
Francisco Soto <sotofloresfrancisco@gmail.com>
2024-02-08 19:15:00 -0500

    mejorando el menu

--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -13,6 +13,10 @@ const NavigationBar = () => {
   const toggleMenu = () => {
     setMenuVisible(!isMenuVisible);
   };
+  const closeMenu = () => {
+    setMenuVisible(false);
+  };
+
   return (
       <nav className="fixed w-full z-20 top-0 start-0 backdrop-blur">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 lg:py-4 lg:px-20">
@@ -30,11 +34,11 @@ const NavigationBar = () => {
           </div>
           <div className={`per items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMenuVisible ? '' : 'hidden'}`} id="navbar-sticky">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-inherit text-black">
-              <NavLinkItem to="/" text="Inicio" />
-              <NavLinkItem to="/about" text="Sobre Mí" />
-              <NavLinkItem to="/proyectos" text="Proyectos" />
+              <NavLinkItem to="/" text="Inicio" onClick={closeMenu}/>
+              <NavLinkItem to="/about" text="Sobre Mí" onClick={closeMenu} />
+              <NavLinkItem to="/proyectos" text="Proyectos" onClick={closeMenu} />
               <NavLinkItem to="/blog" text="Blog" />
-              <NavLinkItem to="/contacto" text="contacto" />
+              <NavLinkItem to="/contacto" text="contacto" onClick={closeMenu}/>
             </ul>
           </div>
         </div>
